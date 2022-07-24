8cd7977072f8d2e8ecb23ccf757df9d6e07132fc
lvhuimin <lvhuimin@ymt360.com>
2022-07-25 00:33:10 +0800

    init环境变量

---
+++ spring-debug/src/main/java/lhm/domain/TestMain.java
@@ -0,0 +1,18 @@
+package lhm.domain;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+
+//		System.out.println("Hello Spring！");
+		ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+//		Object bean = classPathXmlApplicationContext.getBean(TestA.class);
+//		System.out.println(bean);
+
+	}
+
+
+
+
+
+}
